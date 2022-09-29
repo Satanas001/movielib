700b06bee6a0bcbb6c4b7f073e6a8fcc569d9141
Kristof <christophecoupigny@gmail.com>
2022-09-29 14:47:11 +0200

    layout of person form

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Person;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,45 @@ class PersonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthDate')
+            ->add('firstname', null, [
+                'row_attr' => [
+                    'class' =>'mb-3'
+                ],
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'text-primary fw-light',
+                ],
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastname', null, [
+                'row_attr' => [
+                    'class' =>'mb-3'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'text-primary fw-light',
+                ],
+                'attr' => [
+                    'placeholder' => 'Nom',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('birthDate', BirthdayType::class, [
+                'row_attr' => [
+                    'class' =>'mb-3'
+                ],
+                'label' => 'Date de naissance',
+                'label_attr' => [
+                    'class' => 'text-primary fw-light',
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'widget' => 'single_text'
+            ])
         ;
     }
 
